5f7ec0b294c873a98167ddddd1c652003e556b7c
ndfrank <102965015+ndfrank@users.noreply.github.com>
2024-01-06 15:47:44 +0800

    Delete rpc directory

--- rpc/netty-consumer/src/main/java/wwz/constant/Constants.java
+++
@@ -1,6 +0,0 @@
-package wwz.constant;
-
-public class Constants {
-    public static final String SERVER_PATH = "/netty";
-
-}
--- rpc/netty-rpc/src/main/java/wwz/netty/constant/Constants.java
+++
@@ -1,6 +0,0 @@
-package wwz.netty.constant;
-
-public class Constants {
-    public static final String SERVER_PATH = "/netty";
-
-}
